1029f421a3414236304b198a5d00acf57fad2310
Richard Krikler <57064056+RichardKrikler@users.noreply.github.com>
2021-09-28 20:41:11 +0200

    Updated iframe to embedded video

--- index.php
+++ index.php
@@ -2,17 +2,27 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title></title>
+    <title>BooTube</title>
     <link rel="stylesheet" href="stylesheet.css" type="text/css">
 </head>
 <body>
-<section id="s1">
-<?php
-for ($i = 0; $i < 20; $i++) {
-    echo '<h3>Dark Phoenix Song | This Fire Inside</h3>';
-    echo '<iframe src="https://www.youtube.com/watch?v=gyciPq_lD9U" title="Dark Phoenix Song | This Fire Inside" allow="autoplay"></iframe><br>';
-}
-?>
-</section>
+<main>
+    <?php
+    for ($i = 0; $i < 20; $i++) {
+        echo '<h3>Dark Phoenix Song | This Fire Inside</h3>';
+        echo '<h4>Source: YouTube</h4>';
+        echo <<<VIDEOCODE
+            <iframe
+                width="560" height="315"
+                src="https://www.youtube.com/embed/gyciPq_lD9U"
+                title="Dark Phoenix Song | This Fire Inside"
+                frameborder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowfullscreen>
+            </iframe>
+VIDEOCODE;
+    }
+    ?>
+</main>
 </body>
 </html>
