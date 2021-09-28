7b978199848f30e9fb515c2b124c778983955cf0
Jakob Mucherl <jakob.mucherl@htl.rennweg.at>
2021-09-28 19:25:05 +0200

    Added some CSS Code for Styling finished with User Story 1

--- index.php
+++ index.php
@@ -2,9 +2,17 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>BooTube</title>
+    <title></title>
+    <link rel="stylesheet" href="stylesheet.css" type="text/css">
 </head>
 <body>
-
+<section id="s1">
+<?php
+for ($i = 0; $i < 20; $i++) {
+    echo '<h3>Dark Phoenix Song | This Fire Inside</h3>';
+    echo '<iframe src="https://www.youtube.com/watch?v=gyciPq_lD9U" title="Dark Phoenix Song | This Fire Inside" allow="autoplay"></iframe><br>';
+}
+?>
+</section>
 </body>
 </html>
