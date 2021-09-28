b2964d6e33cff39c2aace724f9f33d54bac1c72b
Richard Krikler <57064056+RichardKrikler@users.noreply.github.com>
2021-09-29 00:23:13 +0200

    Added Template Class for basic HTML structure

---
+++ Template/Template.php
@@ -0,0 +1,28 @@
+<?php
+
+class Template
+{
+    static function render(string $main): string
+    {
+        $template_head = <<<TEMPLATE_HEAD
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>BooTube</title>
+    <link rel="stylesheet" href="stylesheet.css" type="text/css">
+</head>
+<body>
+<main>
+TEMPLATE_HEAD;
+
+        $template_footer = <<<TEMPLATE_FOOTER
+</main>
+</body>
+</html>
+TEMPLATE_FOOTER;
+
+        return $template_head . $main . $template_footer;
+    }
+
+}
