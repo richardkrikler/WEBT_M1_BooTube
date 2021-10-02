381b24f13e433f7eef9b942782acf307a884907d
Richard Krikler <57064056+RichardKrikler@users.noreply.github.com>
2021-10-02 10:45:47 +0200

    Added Vimeo Class for rendering Vimeo videos

--- Video/Vimeo.php
+++ Video/Vimeo.php
@@ -2,7 +2,29 @@
 
 namespace WEBT_M1_BooTube\Video;
 
-class Vimeo
+require_once 'AbstractVideo.php';
+
+class Vimeo extends AbstractVideo
 {
+    /**
+     * @param string $name Name of the video
+     * @param string $source Source URL of the video
+     */
+    public function __construct(string $name, string $source)
+    {
+        parent::__construct($name, $source);
+    }
+
+    public function getHTMLOutput(): string
+    {
+        return <<<VIDEOCODE
+        <iframe
+            src="{$this->getSource()}?title=0&byline=0&portrait=0"
+            frameborder="0"
+            allow="autoplay; fullscreen; picture-in-picture"
+            allowfullscreen></iframe>
+VIDEOCODE;
+    }
+
 
 }
--- index.php
+++ index.php
@@ -3,28 +3,30 @@
 namespace WEBT_M1_BooTube;
 
 use WEBT_M1_BooTube\Template\Template;
+use WEBT_M1_BooTube\Video\Vimeo;
 use WEBT_M1_BooTube\Video\Youtube;
 
-require_once "Template/Template.php";
+require_once 'Template/Template.php';
 require_once 'Video/Youtube.php';
+require_once 'Video/Vimeo.php';
 
-$videos[] = new Youtube("This Fire Inside", "https://www.youtube.com/embed/gyciPq_lD9U");
-$videos[] = new Youtube("Champion", "https://www.youtube.com/embed/4ePUkps416s");
-$videos[] = new Youtube("Radioactive", "https://www.youtube.com/embed/ktvTqknDobU");
-$videos[] = new Youtube("Blaues Licht", "https://www.youtube.com/embed/0NL8H1IAHVc");
-$videos[] = new Youtube("Wenn du mich siehst", "https://www.youtube.com/embed/mbrsAiSJjj8");
+$videos[] = new Youtube('This Fire Inside', 'https://www.youtube.com/embed/gyciPq_lD9U');
+$videos[] = new Youtube('Champion', 'https://www.youtube.com/embed/4ePUkps416s');
+$videos[] = new Youtube('Radioactive', 'https://www.youtube.com/embed/ktvTqknDobU');
+$videos[] = new Youtube('Blaues Licht', 'https://www.youtube.com/embed/0NL8H1IAHVc');
+$videos[] = new Youtube('Wenn du mich siehst', 'https://www.youtube.com/embed/mbrsAiSJjj8');
+$videos[] = new Vimeo('Hearing Tarantino', 'https://player.vimeo.com/video/118431867');
+$videos[] = new Vimeo('CONVERGENCE STATION', 'https://player.vimeo.com/video/608932905');
+$videos[] = new Vimeo('88:88', 'https://player.vimeo.com/video/31963455');
+$videos[] = new Vimeo('Neurosymphony', 'https://player.vimeo.com/video/368065919');
+$videos[] = new Vimeo('Spooky Scary', 'https://player.vimeo.com/video/7251379');
 
-$main = '<div id="page-title">';
-$main .= '<h1>BooTube</h1>';
-$main .= '<h2>The scary video portal.</h2>';
-$main .= '</div>';
-
-
-$main .= '<ul>';
+$main = '<ul>';
 foreach ($videos as $video) {
     $main .= '<li>';
     $main .= '<h3>' . $video->getName() . '</h3>';
     $main .= $video->getHTMLOutput();
+    $main .= '<a href="' . $video->getSource() . '" target="_blank">' . $video->getSource() . '</a>';
     $main .= '</li>';
 }
 $main .= '</ul>';
