598671d9d74c4d1cd00f513fc158521e4273658b
Richard Krikler <57064056+RichardKrikler@users.noreply.github.com>
2021-10-07 13:17:32 +0200

    Updated Styling (Font, ...); Added Bootstrap-CSS-Tag

--- Template/Template.php
+++ Template/Template.php
@@ -11,7 +11,11 @@ class Template
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>BooTube</title>
+    
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-uWxY/CJNBR+1zjPWmfnSnVxwRheevXITnMqoEIeG1LJrdI0GlVs/9cVSyPYXdcSF" crossorigin="anonymous">
     <link rel="stylesheet" href="stylesheet.css" type="text/css">
 </head>
 <body>
